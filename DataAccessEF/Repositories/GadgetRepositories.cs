90a19c8f18f453bab305076b9fc2816d413eac4c
Omlet144 <alexblack144@gmail.com>
2023-02-27 00:41:40 +0200

    All Project

---
+++ DataAccessEF/Repositories/GadgetRepositories.cs
@@ -0,0 +1,28 @@
+﻿using DataAccessEF.Data;
+using Domain.Interfaces;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessEF.Repositories
+{
+    public class GadgetRepositories : GenericRepository<Gadget>, IGadgetRepository
+    {
+        public GadgetRepositories(asp_tablesContext dbContext) : base(dbContext)
+        {
+        }
+        public IEnumerable<Gadget> GetAll() => _dbContext.Gadgets.ToList();
+
+        public Gadget GetId(int id)
+        {
+            return _dbContext.Gadgets.Find(id);
+        }
+        public IEnumerable<Gadget> GetbyIdCategory(int id)
+        {
+            return _dbContext.Gadgets.AsEnumerable().Where(x => x.IdCategory == id);
+        }
+    }
+}
---
+++ Domain/Models/Gadget.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models
+{
+    public partial class Gadget
+    {
+        public Gadget()
+        {
+            Users = new HashSet<User>();
+        }
+
+        public int Id { get; set; }
+        public string? Image { get; set; }
+        public string? Name { get; set; }
+        public string? Model { get; set; }
+        public double? Price { get; set; }
+        public int? Quantity { get; set; }
+        public int? Sold { get; set; }
+        public bool? Status { get; set; }
+        public int? IdCategory { get; set; }
+
+        public virtual Category? IdCategoryNavigation { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
